770d1a577e1ddd1d51c48bb4e7ee8c1bf0ce571f
ojaradonatello <ojaradonato4@gmail.com>
2024-10-25 12:28:18 +0300

    product display page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
+import React from 'react'
 function App() {
   return (
-    <h1> Modernshop</h1>
+    <>
+    <Header/>
+     <main className='py-3'>
+      <Container>
+      <Outlet   />
+    </Container>
+    </main>
+    <Footer/>
+    </>
   )
 }
 
